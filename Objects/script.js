0273fbd554d9560ebc82b0019b088fe7f3693297
GlebNekrasov <g_nekrasov@mail.ru>
2022-12-19 00:22:22 +0700

    1. Каждая подзадача оформлена в виде функции. 2. Удалены имена переменных при объявлении элементов массивов внутри объекта, чтобы не создавались глобальные переменные. 3. В названиях полей убраны "приставки" city и country, так как по контексту объявления этих полей и так понятно, к какому объекту относится поле. 4. В конце списка стран удалена лишняя запятая. 5. Добавлена ";" в конце строк там, где это необходимо. 6. Удалена излишняя переменная maxCitiesCountCountriesArray - вместо этого в maxCitiesCountCountries сразу сделано вычисление массива стран с максимальным количеством городов. 7. В функциях даны более понятные имена аргументам. 8. Объекту, в который "складываются" результаты вычислений численности населения стран, дано более подходящее имя countriesPopulation.

--- Objects/script.js
+++ Objects/script.js
@@ -1,117 +1,125 @@
 (function () {
     var countriesArray = [
-        Russia = {
-            countryName: "Россия",
-            countryCities: [
-                Moscow = {
-                    cityName: "Москва",
-                    cityPopulation: 12655050
+        {
+            name: "Россия",
+            cities: [
+                {
+                    name: "Москва",
+                    population: 12655050
                 },
-                Kazan = {
-                    cityName: "Казань",
-                    cityPopulation: 1257341
+                {
+                    name: "Казань",
+                    population: 1257341
                 },
-                Novosibirsk = {
-                    cityName: "Новосибирск",
-                    cityPopulation: 1620162
+                {
+                    name: "Новосибирск",
+                    population: 1620162
                 },
-                Omsk = {
-                    cityName: "Омск",
-                    cityPopulation: 1139897
+                {
+                    name: "Омск",
+                    population: 1139897
                 },
-                Ekaterinburg = {
-                    cityName: "Екатеринбург",
-                    cityPopulation: 1495066
+                {
+                    name: "Екатеринбург",
+                    population: 1495066
                 },
-                Krasnoyarsk = {
-                    cityName: "Красноярск",
-                    cityPopulation: 1092851
+                {
+                    name: "Красноярск",
+                    population: 1092851
                 }
             ]
         },
-        Japan = {
-            countryName: "Япония",
-            countryCities: [
-                Tokyo = {
-                    cityName: "Токио",
-                    cityPopulation: 13953577
+        {
+            name: "Япония",
+            cities: [
+                {
+                    name: "Токио",
+                    population: 13953577
                 },
-                Osaka = {
-                    cityName: "Осака",
-                    cityPopulation: 2685481
+                {
+                    name: "Осака",
+                    population: 2685481
                 },
-                Yokohama = {
-                    cityName: "Йокогама",
-                    cityPopulation: 3681279
+                {
+                    name: "Йокогама",
+                    population: 3681279
                 }
             ]
         },
-        France = {
-            countryName: "Франция",
-            countryCities: [
-                Paris = {
-                    cityName: "Париж",
-                    cityPopulation: 2206488
+        {
+            name: "Франция",
+            cities: [
+                {
+                    name: "Париж",
+                    population: 2206488
                 },
-                Marcel = {
-                    cityName: "Марсель",
-                    cityPopulation: 869815
+                {
+                    name: "Марсель",
+                    population: 869815
                 },
-                Lyon = {
-                    cityName: "Лион",
-                    cityPopulation: 506615
+                {
+                    name: "Лион",
+                    population: 506615
                 },
-                Nice = {
-                    cityName: "Ницца",
-                    cityPopulation: 343895
+                {
+                    name: "Ницца",
+                    population: 343895
                 }
             ]
         },
-        Italy = {
-            countryName: "Италия",
-            countryCities: [
-                Rome = {
-                    cityName: "Рим",
-                    cityPopulation: 2870500
+        {
+            name: "Италия",
+            cities: [
+                {
+                    name: "Рим",
+                    population: 2870500
                 },
-                Milan = {
-                    cityName: "Милан",
-                    cityPopulation: 1378689
+                {
+                    name: "Милан",
+                    population: 1378689
                 },
-                Naples = {
-                    cityName: "Неаполь",
-                    cityPopulation: 956183
+                {
+                    name: "Неаполь",
+                    population: 956183
                 },
-                Turin = {
-                    cityName: "Турин",
-                    cityPopulation: 878735
+                {
+                    name: "Турин",
+                    population: 878735
                 }
             ]
-        },
+        }
     ];
 
-    var maxCitiesCount = Math.max.apply(Math, countriesArray.map(function (item) {
-        return item.countryCities.length
-    }));
+    function findMaxCitiesCountCountries(countries) {
+        var maxCitiesCount = Math.max.apply(null, countries.map(function (country) {
+            return country.cities.length;
+        }));
 
-    var maxCitiesCountCountriesArray = countriesArray.filter(function (item) {
-        return item.countryCities.length === maxCitiesCount;
-    });
+        var maxCitiesCountCountries = countries
+            .filter(function (country) {
+                return country.cities.length === maxCitiesCount;
+            })
+            .map(function (country) {
+                return country.name;
+            })
+        ;
 
-    var maxCitiesCountCountries = maxCitiesCountCountriesArray.map(function (item) {
-        return item.countryName;
-    })
+        console.log("Страны с максимальным количеством городов: " + maxCitiesCountCountries.join(", "));
+    }
 
-    console.log("Страны с максимальным количеством городов: " + maxCitiesCountCountries.join(", "));
+    function getCountriesPopulation(countries) {
+        var countriesPopulation = {};
 
-    var countries = {};
+        countries.forEach(function (country) {
+            var countryName = country.name;
+            countriesPopulation[countryName] = country.cities.reduce(function (citiesPopulation, city) {
+                return citiesPopulation + city.population;
+            }, 0);
+        });
 
-    countriesArray.forEach(function (item) {
-        var countryName = item.countryName;
-        countries[countryName] = item.countryCities.reduce(function (accumulator, item) {
-            return accumulator + item.cityPopulation;
-        }, 0);
-    })
+        console.log("страны и численность населения их городов: ", countriesPopulation);
+    }
 
-    console.log("страны и численность населения их городов: ", countries);
+    findMaxCitiesCountCountries(countriesArray);
+    getCountriesPopulation(countriesArray);
 })();
