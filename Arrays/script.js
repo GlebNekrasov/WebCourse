5ef07ca374845beb54063007d8374a5e644a67cd
GlebNekrasov <g_nekrasov@mail.ru>
2022-12-20 00:19:01 +0700

    1. Теперь в печати в консоль не используются сложные выражения - вместо этого заведены переменные. 2. Выражения с использованием нескольких методов отформатированы для лучшей читаемости. 3. Все массивы выводятся в консоль с использованием join. 4. Именам массивов и переменных даны подходящие имена. 5. Код оформлен в виде функций. При необходимости эти функции передают результат вычислений в новую переменную. В данном случае такие переменные не заведены, так как они дальше нигде не используются. 6. Аргументам функций даны более подходящие имена.

--- Arrays/script.js
+++ Arrays/script.js
@@ -1,47 +1,86 @@
-(function() {
-    var array1 = [54, 32, 17, 1340, 45, 237, 22209, 1, -18, 102, 2743, 65, 42, 32, 17, 257, 24, 678, 37, 389, 140];
+(function () {
+    function sortDescending(numbersArray) {
+        numbersArray.sort(function (number1, number2) {
+            return number2 - number1;
+        });
 
-    console.log("Исходный массив:");
-    console.log(array1.join(", "));
+        console.log("Массив, отсортированный по убыванию:");
+        console.log(numbersArray.join(", "));
+    }
 
-    array1.sort(function (e1, e2) {
-        return e2 - e1;
-    });
-
-    console.log("Исходный массив, отсортированный по убыванию:");
-    console.log(array1.join(", "));
-
-    var array2 = array1.slice(0, 5);
-    var array3 = array1.slice(array1.length - 5);
-
-    console.log("Подмассив из первых 5 элементов исходного массива, отсортированного по убыванию:");
-    console.log(array2.join(", "));
-    console.log("Подмассив из последних 5 элементов исходного массива, отсортированного по убыванию:");
-    console.log(array3.join(", "));
-
-    console.log("Сумма четных чисел массива равна:");
-    console.log(array1.reduce(function (sum, item) {
-        if (item % 2 === 0) {
-            return sum + item;
+    function getFirstFiveNumbers(numbersArray) {
+        if (numbersArray.length >= 5) {
+            var firstFiveNumbersArray = numbersArray.slice(0, 5);
+            console.log("Подмассив из первых 5 элементов массива:");
+            console.log(firstFiveNumbersArray.join(", "));
+            return firstFiveNumbersArray;
         }
 
-        return sum;
-    }, 0));
+        console.log("В исходном массиве меньше 5 элементов:");
+        console.log(numbersArray.join(", "));
+        return numbersArray;
+    }
 
-    var array4 = [];
+    function getLastFiveNumbers(numbersArray) {
+        if (numbersArray.length >= 5) {
+            var lastFiveNumbersArray = numbersArray.slice(numbersArray.length - 5);
+            console.log("Подмассив из последних 5 элементов массива:");
+            console.log(lastFiveNumbersArray.join(", "));
+            return lastFiveNumbersArray;
+        }
+
+        console.log("В исходном массиве меньше 5 элементов:");
+        console.log(numbersArray.join(", "));
+        return numbersArray;
+    }
+
+    function getEvenNumbersSum(numbersArray) {
+        var evenNumbersSum = numbersArray.reduce(function (sum, number) {
+            if (number % 2 === 0) {
+                return sum + number;
+            }
+
+            return sum;
+        }, 0);
+
+        console.log("Сумма четных чисел массива равна: " + evenNumbersSum);
+
+        return evenNumbersSum;
+    }
+
+    function getEvenNumbersSquares(numbersArray) {
+        var evenNumbersSquaresArray = numbersArray.filter(function (number) {
+            return number % 2 === 0;
+        }).map(function (number) {
+            return number * number;
+        });
+
+        console.log("Список квадратов четных чисел массива:");
+        console.log(evenNumbersSquaresArray.join(", "));
+        return evenNumbersSquaresArray;
+    }
+
+    var randomNumbersArray = [54, 32, 17, 1340, 45, 237, 22209, 1, -18, 102, 2743, 65, 42, 32, 17, 257, 24, 678, 37, 389, 140];
+
+    console.log("Исходный массив:");
+    console.log(randomNumbersArray.join(", "));
+
+    sortDescending(randomNumbersArray);
+
+    getFirstFiveNumbers(randomNumbersArray);
+
+    getLastFiveNumbers(randomNumbersArray);
+
+    getEvenNumbersSum(randomNumbersArray);
+
+    var from1To100Array = [];
 
     for (var i = 1; i <= 100; ++i) {
-        array4.push(i);
+        from1To100Array.push(i);
     }
 
     console.log("Создан массив чисел от 1 до 100:");
-    console.log(array4);
+    console.log(from1To100Array.join(", "));
 
-    var array5 = array4.filter(function (item) {
-        return item % 2 === 0;
-        }).map(function (item) {
-            return item * item;
-    });
-
-    console.log("Список квадратов четных чисел массива: " + array5.join(", "));
+    getEvenNumbersSquares(from1To100Array);
 })();
