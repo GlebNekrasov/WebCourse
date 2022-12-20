f06c197895f03436aaef494f06499ccb2f9b52ec
GlebNekrasov <g_nekrasov@mail.ru>
2022-12-20 22:43:40 +0700

    1. Замене селектора для непосредственных потомков <body> на сам <body> при навешивании стилей шрифта на весь документ. 2. Применение padding'ов навешено только на те элементы, в которых это требуется. 3. Исправлена ошибка в названии селектора "big-header". 4. Объявление переменных temperatureInputField, errorMessage вынесено во внешнюю функцию, чтобы не искать значения этих переменных каждый раз нажатии на кнопку. 5. Удалена излишняя переменная inputTemperature. 6. Добавлено использование переменной temperatureInputField там, где раньше заново вычислялось значение, соответствующее значению этой переменной. 7. Создана вспомогательная функция по созданию блока с выходными данными в какой-либо шкале температур. 8. Удалено использование самовызывающейся функции, так как код уже помещен в анонимную функцию.

--- TemperatureConverter/script.js
+++ TemperatureConverter/script.js
@@ -1,64 +1,48 @@
-(function () {
-    document.addEventListener("DOMContentLoaded", function () {
-        var button = document.querySelector(".conversion-button");
+document.addEventListener("DOMContentLoaded", function () {
+    var button = document.querySelector(".conversion-button");
+    var temperatureInputField = document.getElementById("input-temperature");
+    var errorMessage = document.querySelector(".error");
 
-        button.addEventListener("click", function (e) {
-            e.preventDefault();
+    button.addEventListener("click", function (e) {
+        e.preventDefault();
 
-            var outputNode = document.querySelector(".output-container");
+        var outputNode = document.querySelector(".output-container");
 
-            if (outputNode !== null) {
-                outputNode.remove();
+        if (outputNode !== null) {
+            outputNode.remove();
+        }
+
+        var celsiusTemperature = parseFloat(temperatureInputField.value);
+
+        if (isNaN(celsiusTemperature)) {
+            temperatureInputField.classList.add("invalid-field");
+            errorMessage.style.display = "block";
+        } else {
+            temperatureInputField.classList.remove("invalid-field");
+            temperatureInputField.value = "";
+            errorMessage.style.display = "none";
+
+            var outputDiv = document.createElement("div");
+            outputDiv.classList.add("output-container");
+            document.body.appendChild(outputDiv);
+
+            function addConvertedTemperature (scaleDescription, temperature) {
+                var temperatureValue = document.createElement("span");
+                temperatureValue.textContent = temperature.toString();
+                temperatureValue.classList.add("temperature-value");
+
+                var temperatureParagraph = document.createElement("p");
+                temperatureParagraph.textContent = scaleDescription;
+                outputDiv.appendChild(temperatureParagraph);
+                temperatureParagraph.appendChild(temperatureValue);
             }
 
-            var temperatureInputField = document.getElementById("input-temperature");
-            var inputTemperature = temperatureInputField.value;
-            var celsiusTemperature = parseFloat(inputTemperature);
-            var errorMessage = document.querySelector(".error");
+            var kelvinTemperature = celsiusTemperature + 273.15;
+            var fahrenheitTemperature = celsiusTemperature * 9 / 5 + 32;
 
-            if (isNaN(celsiusTemperature)) {
-                temperatureInputField.classList.add("invalid-field");
-                errorMessage.style.display = "block";
-            } else {
-                temperatureInputField.classList.remove("invalid-field");
-                errorMessage.style.display = "none";
-                document.getElementById("input-temperature").value = "";
-
-                var outputDiv = document.createElement("div");
-                outputDiv.classList.add("output-container");
-                document.body.appendChild(outputDiv);
-
-                var celsiusValue = document.createElement("span");
-                celsiusValue.textContent = celsiusTemperature.toString();
-                celsiusValue.classList.add("temperature-value");
-
-                var celsiusParagraph = document.createElement("p");
-                celsiusParagraph.textContent = "Температура в градусах Цельсия: ";
-                outputDiv.appendChild(celsiusParagraph);
-                celsiusParagraph.appendChild(celsiusValue);
-
-                var kelvinTemperature = celsiusTemperature + 273.15;
-
-                var kelvinValue = document.createElement("span");
-                kelvinValue.textContent = kelvinTemperature.toString();
-                kelvinValue.classList.add("temperature-value");
-
-                var kelvinParagraph = document.createElement("p");
-                kelvinParagraph.textContent = "Температура в Кельвинах: ";
-                outputDiv.appendChild(kelvinParagraph);
-                kelvinParagraph.appendChild(kelvinValue);
-
-                var fahrenheitTemperature = celsiusTemperature * 9 / 5 + 32;
-
-                var fahrenheitValue = document.createElement("span");
-                fahrenheitValue.textContent = fahrenheitTemperature.toString();
-                fahrenheitValue.classList.add("temperature-value");
-
-                var fahrenheitParagraph = document.createElement("p");
-                fahrenheitParagraph.textContent = "Температура в Фаренгейтах: ";
-                outputDiv.appendChild(fahrenheitParagraph);
-                fahrenheitParagraph.appendChild(fahrenheitValue);
-            }
-        });
+            addConvertedTemperature("Температура в градусах Цельсия: ", celsiusTemperature);
+            addConvertedTemperature("Температура в Кельвинах: ", kelvinTemperature);
+            addConvertedTemperature("Температура в Фаренгейтах: ", fahrenheitTemperature);
+        }
     });
-})();
+});
