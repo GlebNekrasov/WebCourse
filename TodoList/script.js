341959cc676f49c6989e8dccda41074c8a797eae
GlebNekrasov <g_nekrasov@mail.ru>
2022-12-28 23:36:34 +0700

    1. Добавлен вертикальный отступ снизу у блока с формой добавления новой задачи - чтобы на малых экранах кнопка добавления не "прилипала" к полю ввода. 2. В html-файле отформатирована разметка по Alt+Ctrl+L. 3. Название CSS-класса bigHeader исправлено с учетом принятой нотации на big-header. 4. Переменные newTaskField, errorMessage, tasksList вынесены из обработчика кнопки добавления новой задачи - чтобы не вчислять эти переменные при каждом клике на кнопку. 5. При использовании innerHTML для лучшей читаемости заменены двойные кавычки на одинарные, чтобы избежать экранирования кавычек. 6. Добавлено использование переменной editTaskField там, где раньше заново вычислялось такое же выражение, как в этой переменной. 7. Удалено использование самовызывающейся функции, так как весь код и так находится внутри анонимной функции. 8. Добавлены точки с запятой там, где это необходимо.

--- TodoList/script.js
+++ TodoList/script.js
@@ -1,74 +1,72 @@
-(function () {
-    document.addEventListener("DOMContentLoaded", function () {
-        var addButton = document.querySelector(".add-button");
+document.addEventListener("DOMContentLoaded", function () {
+    var addButton = document.querySelector(".add-button");
+    var newTaskField = document.getElementById("new-task");
+    var errorMessage = document.querySelector(".error");
+    var tasksList = document.querySelector(".tasks-list");
 
-        addButton.addEventListener("click", function (e) {
-            e.preventDefault();
+    addButton.addEventListener("click", function (e) {
+        e.preventDefault();
 
-            var newTaskField = document.getElementById("new-task");
-            var newTaskText = newTaskField.value.trim();
-            var errorMessage = document.querySelector(".error");
-            var tasksList = document.querySelector(".tasks-list");
+        var newTaskText = newTaskField.value.trim();
 
-            if (newTaskText.length === 0) {
-                newTaskField.classList.add("invalid-field");
-                errorMessage.style.display = "block";
-                return;
-            }
+        if (newTaskText.length === 0) {
+            newTaskField.classList.add("invalid-field");
+            errorMessage.style.display = "block";
+            return;
+        }
 
-            newTaskField.classList.remove("invalid-field");
-            errorMessage.style.display = "none";
-            newTaskField.value = "";
+        newTaskField.classList.remove("invalid-field");
+        errorMessage.style.display = "none";
+        newTaskField.value = "";
 
-            var newTask = document.createElement("li");
-            newTask.classList.add("task");
+        var newTask = document.createElement("li");
+        newTask.classList.add("task");
 
-            tasksList.appendChild(newTask);
+        tasksList.appendChild(newTask);
 
-            function setViewMode() {
-                newTask.innerHTML = "<span></span>\
+        function setViewMode() {
+            newTask.innerHTML = "<span></span>\
                     <button class='delete-button' type='button'>Удалить</button>\
                     <button class='edit-button' type='button'>Редактировать</button>";
 
-                newTask.firstChild.textContent = newTaskText;
+            newTask.firstChild.textContent = newTaskText;
 
-                newTask.querySelector(".delete-button").addEventListener("click", function () {
-                    newTask.remove();
-                });
+            newTask.querySelector(".delete-button").addEventListener("click", function () {
+                newTask.remove();
+            });
 
-                newTask.querySelector(".edit-button").addEventListener("click", function () {
-                    setEditMode();
-                });
-            }
+            newTask.querySelector(".edit-button").addEventListener("click", function () {
+                setEditMode();
+            });
+        }
 
-            function setEditMode() {
-                newTask.innerHTML = "<input class='edit-task' type='text'>\
+        function setEditMode() {
+            newTask.innerHTML = "<input class='edit-task' type='text'>\
                     <button class='save-button' type='button'>Сохранить</button>\
                     <button class='cancel-button' type='button'>Отменить</button>\
-                    <div class=\"error\">Нужно заполнить поле!</div>";
+                    <div class='error'>Нужно заполнить поле!</div>";
 
-                var editTaskField = newTask.querySelector(".edit-task");
-                editTaskField.value = newTaskText;
+            var editTaskField = newTask.querySelector(".edit-task");
+            editTaskField.value = newTaskText;
 
-                newTask.querySelector(".cancel-button").addEventListener("click", function () {
-                    setViewMode();
-                })
+            newTask.querySelector(".cancel-button").addEventListener("click", function () {
+                setViewMode();
+            });
 
-                newTask.querySelector(".save-button").addEventListener("click", function () {
-                    var editedTaskText = newTask.querySelector(".edit-task").value.trim();
+            newTask.querySelector(".save-button").addEventListener("click", function () {
+                var editedTaskText = editTaskField.value.trim();
 
-                    if (editedTaskText.length === 0) {
-                        newTask.querySelector(".error").style.display = "block";
-                        editTaskField.classList.add("invalid-field");
-                        return;
-                    }
+                if (editedTaskText.length === 0) {
+                    newTask.querySelector(".error").style.display = "block";
+                    editTaskField.classList.add("invalid-field");
+                    return;
+                }
 
-                    newTaskText = editedTaskText;
-                    setViewMode();
-                })
-            }
+                newTaskText = editedTaskText;
+                setViewMode();
+            });
+        }
 
-            setViewMode();
-        });
+        setViewMode();
     });
-})();
+});
