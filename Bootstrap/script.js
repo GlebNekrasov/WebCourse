add055fa2e07b0a1d0b7487fdaa4c5b90b7a3afa
GlebNekrasov <g_nekrasov@mail.ru>
2023-02-07 23:49:31 +0700

    1. В файле script.js отформатирован код. 2. Расположение всех кнопок унифицировано для разных экранов: - на малых экранах все кнопки размещаются под элементом, к которому они относятся, и растягиваются на всю ширину экрана; - на остальных экранах кнопки располагаются справа от элемента, к которому они относятся. 3. В package.json добавлены все стандартные поля. 4. Цвета у кнопок сделаны более подходящими: - у кнопок удаления сделан красный цвет; - у кнопок сохранения сделан синий цвет, как у кнопки добавления задачи. 5. Изменена последовательность кнопок - теперь сначала идет кнопка редактирования, а потом кнопка удаления. 6. У body удалены отступы, так как класс container автоматически добавляет отступы. 7. Удалено использование избыточного класса error, так как возможностей встроенного класса invalid-feedback оказалось достаточно.

--- Bootstrap/script.js
+++ Bootstrap/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     var newTaskField = $("#new-task");
-    var errorMessage = $(".error");
+    var errorMessage = $(".invalid-feedback");
     var tasksList = $(".tasks-list");
 
     $(".add-button").click(function (e) {
@@ -10,24 +10,26 @@ $(document).ready(function () {
 
         if (newTaskText.length === 0) {
             newTaskField.addClass("is-invalid");
-            errorMessage.addClass("invalid-feedback");
+            errorMessage.show();
             return;
         }
 
-        errorMessage.removeClass("invalid-feedback");
+        errorMessage.hide();
         newTaskField.removeClass("is-invalid");
         newTaskField.val("");
 
-        var newTask = $("<li class='list-group-item text-success text-break px-0 border-0 mb-2 py-0'></li>");
+        var newTask = $("<li class='list-group-item d-sm-flex align-items-center gap-2 text-success px-0 py-2'></li>");
 
         tasksList.append(newTask);
 
         function setViewMode() {
-            newTask.html("<span></span>\
-                <button class='delete-button btn btn-secondary btn-sm' type='button'>Удалить</button>\
-                <button class='edit-button btn btn-secondary btn-sm' type='button'>Редактировать</button>");
+            newTask.html("<span class='task-text text-break'></span>\
+                <div class='row flex-nowrap mt-2 mt-sm-0'>\
+                <div class='row-cols-1 col-6 col-sm-auto'><button class='edit-button btn btn-secondary btn-sm' type='button'>Редактировать</button></div>\
+                <div class='row-cols-1 col-6 col-sm-auto'><button class='delete-button btn btn-danger btn-sm' type='button'>Удалить</button></div>\
+                </div>");
 
-            newTask.children(":first").text(newTaskText);
+            newTask.find(".task-text").text(newTaskText);
 
             newTask.find(".delete-button").click(function () {
                 newTask.remove();
@@ -39,13 +41,27 @@ $(document).ready(function () {
         }
 
         function setEditMode() {
-            newTask.html("<input class='edit-task form-control mb-2' type='text'>\
-                    <div class='error mb-2'>Нужно заполнить поле!</div>\
-                    <button class='save-button btn btn-secondary btn-sm' type='button'>Сохранить</button>\
-                    <button class='cancel-button btn btn-secondary btn-sm' type='button'>Отменить</button>");
+            newTask.html("<div class='d-sm-flex d-inline-block col-sm col-11 align-items-baseline gap-2 text-success px-0'>\
+                <div class='col-sm'>\
+                <div class='row'>\
+                <div class='col-12'>\
+                <input class='edit-task form-control' type='text'>\
+                </div>\
+                <div class='invalid-feedback col-12'>Нужно заполнить поле!</div>\
+                </div>\
+                </div>\
+                <div class='col-12 col-sm-auto'>\
+                <div class='row flex-nowrap mt-2 mt-sm-0'>\
+                <div class='row-cols-1 col-6 col-sm-auto'><button class='save-button btn btn-primary btn-sm' type='button'>Сохранить</button></div>\
+                <div class='row-cols-1 col-6 col-sm-auto'><button class='cancel-button btn btn-secondary btn-sm' type='button'>Отменить</button></div>\
+                </div>\
+                </div>\
+                </div>");
 
             var editTaskField = newTask.find(".edit-task");
             editTaskField.val(newTaskText);
+
+            var editErrorMessage = newTask.find(".invalid-feedback");
 
             newTask.find(".cancel-button").click(function () {
                 setViewMode();
@@ -55,8 +71,8 @@ $(document).ready(function () {
                 var editedTaskText = editTaskField.val().trim();
 
                 if (editedTaskText.length === 0) {
-                    newTask.find(".error").addClass("invalid-feedback");
                     editTaskField.addClass("is-invalid");
+                    editErrorMessage.show();
                     return;
                 }
 
